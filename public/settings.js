982a891d0e01148dd68464f033ade2f778f5de0e
OreoDivision <Bobgim20@gmail.com>
2021-04-02 11:54:16 -0500

    Adds a delete button

--- public/settings.js
+++ public/settings.js
@@ -1,5 +1,5 @@
 const socket = io('http://localhost:3000')
-const settingsForm = document.getElementById('settings');
+const publicSwitch = document.getElementById('public-switch');
 const back = document.getElementById('back')
 const saved = document.getElementById('saved')
 const deleteRoom = document.getElementById('delete')
@@ -15,9 +15,10 @@ deleteRoom.addEventListener('click', () => {
 		return;
 	}
 	socket.emit('delete-room', roomName)
+	window.location.href = '/';
 })
 
-settingsForm.addEventListener('submit', e => {
+publicSwitch.addEventListener('submit', e => {
 	e.preventDefault()
 	socket.emit('privacy-change', roomName, document.getElementById('private').value)
 	saved.innerText = "Saved"
--- server.js
+++ server.js
@@ -61,6 +61,13 @@ server.listen(3000)
 io.on('connection', socket => {
 	slowmode = 0
 
+	socket.on('delete-room', room => {
+		for(id in rooms[room].users){
+			socket.to(id).emit('redirect', '/')
+		}
+		delete rooms[room];
+	})
+
 	socket.on("check-name", (name, room) => {
 		for (id in rooms[room].users){
 			if (rooms[room].users[id] == name && id != socket.id) return socket.emit('sendback-name', false);
@@ -87,7 +94,7 @@ io.on('connection', socket => {
 		}
 	})
 	socket.on('send-chat-message', (room, message) => {
-		if (slowmode == 1){
+		if (slowmode == 1 && socket.id != rooms[room].owner){
 			socket.emit('kick-success', `You can send a message every 1 second`)
 			return;
 		}
