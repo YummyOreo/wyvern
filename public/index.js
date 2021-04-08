556b74fc72c411113f75ec3b3e9a9fae5e208cc8
OreoDivision <Bobgim20@gmail.com>
2021-04-08 08:33:26 -0500

    Fixes the .gitignore

--- public/index.js
+++
@@ -1,5 +0,0 @@
-const roomsButton = document.getElementById('rooms');
-//redirects to the room link when clicked
-roomsButton.addEventListener('click', () => {
-    window.location.href = '/home/rooms';
-});
--- public/rooms.js
+++
@@ -1,5 +0,0 @@
-const homeButton = document.getElementById('home');
-//redirects to the homebutton link when clicked
-homeButton.addEventListener('click', () => {
-    window.location.href = '/home';
-});
